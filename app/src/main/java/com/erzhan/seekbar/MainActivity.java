c2658d5c95d079b682dc959f5a0c60dbc94b321c
sydykov77 <erjansydykov77>
2020-04-27 19:17:55 +0630

    Разница

--- app/src/main/java/com/erzhan/seekbar/MainActivity.java
+++ app/src/main/java/com/erzhan/seekbar/MainActivity.java
@@ -2,8 +2,8 @@ package com.erzhan.seekbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
@@ -13,11 +13,11 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     SeekBar seekBar;
-    TextView score,seekBarText;
+    TextView score, seekBarText;
     TextView answer;
     Button button;
-    static int scoreInt;
-    Integer x;
+    static int scoreInt = 0;
+    int x, differ, differ1;
     final Random random = new Random();
 
     @Override
@@ -30,19 +30,29 @@ public class MainActivity extends AppCompatActivity {
         score = findViewById(R.id.score);
         answer = findViewById(R.id.answer);
         button = findViewById(R.id.click);
-        seekBarText=findViewById(R.id.seekBarText);
+        seekBarText = findViewById(R.id.seekBarText);
+        x = random.nextInt((100) + 1);
+        differ = x - 10;
+        differ1 = x + 10;
+
+        Log.d("ololo", "otvet: " + differ);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (seekBar.getProgress() == random.nextInt(101)) {
+                if (x == seekBar.getProgress()) {
+                    answer.setText("Сорвал КУШ! \n + 500 очков");
                     scoreInt = scoreInt + 500;
-                    answer.setText("В точку!+ 500 очков!");
+                    score.setText(String.valueOf(scoreInt));
+                } else if (differ <= seekBar.getProgress() && differ1 >= seekBar.getProgress()) {
+                    answer.setText("Близко,но мимо \n +50 очков");
+                    scoreInt = scoreInt + 50;
+                    score.setText(String.valueOf(scoreInt));
                 } else {
-                    scoreInt = scoreInt - 50;
-                    answer.setText("Попробуй еще раз!");
+                    answer.setText("Не угадал \n -10 очков");
+                    scoreInt = scoreInt - 10;
+                    score.setText(String.valueOf(scoreInt));
                 }
-
             }
         });
 
@@ -50,8 +60,7 @@ public class MainActivity extends AppCompatActivity {
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                seekBarText.setText("" + progress );
-
+                seekBarText.setText("" + progress);
 
 
             }
@@ -68,12 +77,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
 
     }
 
